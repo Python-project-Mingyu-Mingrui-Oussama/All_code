e37b5e459bfafd6dba15d6cc4c82e46769b5e276
AkiyamaKoyomi <157613226+AkiyamaKoyomi@users.noreply.github.com>
2024-01-28 17:41:14 +0100

    Add files via upload

--- data.py
+++ data.py
@@ -76,6 +76,17 @@ def extract_all_location(id_odsp):
         location_objects.append(location_object)
     return location_objects
 
+
+def extract_all_pass(location_objects):
+    for item in location_objects:
+        if not np.isnan(item.location):
+            location_objects.remove(item)
+    return location_objects
+
+
+
+
+
 def calculate_attack_distribution(location_objects):
     '''
     Parameters
@@ -85,6 +96,53 @@ def calculate_attack_distribution(location_objects):
     Returns
     -------
     output : a matrix which describes the attack distribution of home team and away team
+
+    '''
+    #initialization of matrix
+    output = np.array([[0, 0],             #left away team & home team
+                   [0, 0],                 #center away team & home team
+                   [0, 0]],  dtype=float)  #right away team & home team
+
+    #calculate different location appearance
+    #board = np.empty((2, 19), dtype=float)
+    board = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], #home team
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]] #away team
+    for obj in location_objects:
+        if obj.side == 1:
+            board[0][int(obj.location)-1] +=1
+        if obj.side == 2:
+            board[1][int(obj.location)-1] +=1
+
+
+    output[0][1]= board[0][0]*0.2 + board[0][3] + board[0][8] + board[0][6] + board[0][9]                               #home team left
+    output[1][1]= board[0][0]*0.6 + board[0][3] + board[0][12] + board[0][13] + board[0][5]+ board[0][15]+ board[0][14] #home team center
+    output[2][1]= board[0][0]*0.2 + board[0][10] + board[0][4] + board[0][7] + board[0][11]                             #home team right
+    output[0][0]= board[1][0]*0.2 + board[1][3] + board[1][8] + board[1][6] + board[1][9]                               #home team left
+    output[1][0]= board[1][0]*0.6 + board[1][3] + board[1][12] + board[1][13] + board[1][5]+ board[1][15]+ board[1][14] #home team center
+    output[2][0]= board[1][0]*0.2 + board[1][10] + board[1][4] + board[1][7] + board[1][11]                             #home team right
+    #uniformization home team
+    a=output[1][1]+output[0][1]+output[2][1]
+    output[0][1]= output[0][1]/a
+    output[1][1]= output[1][1]/a
+    output[2][1]= output[2][1]/a
+    #uniformization away team
+    b=output[1][0]+output[0][0]+output[2][0]
+    output[0][0]= output[0][0]/b
+    output[1][0]= output[1][0]/b
+    output[2][0]= output[2][0]/b
+
+    
+    return output
+
+def calculate_pass_distribution(location_objects):
+    '''
+    Parameters
+    ----------
+    location_objects : list of object Event
+
+    Returns
+    -------
+    output : a matrix which describes the pass distribution of home team and away team
 
     '''
     #initialization of matrix
@@ -213,61 +271,33 @@ def extract_two_team(home_team, away_team):
     return match_history
 
 
-def pourcentage_victoire(home_team, away_team, historique_rencontres):
-    # Calcul du pourcentage en se basant sur l'historique, les matchs gagnés et les buts marqués
-    pourcentage_home = 0.6  # Valeur initiale pour l'équipe à domicile (peut être ajustée)
 
-    # Influence de l'historique des rencontres
-    for match in historique_rencontres:
-        if match.ht == home_team:
-            if match.htscore > match.atscore:
-                pourcentage_home += 0.1  # Ajouter 0.1 si l'équipe à domicile a gagné ce match dans l'historique
-            if match.htscore < match.atscore:
-                pourcentage_home -= 0.1  # Soustraire 0.1 si l'équipe à domicile a perdu ce match dans l'historique
-        if match.at == home_team:
-            if match.htscore < match.atscore:
-                pourcentage_home += 0.1  # Ajouter 0.1 si l'équipe à domicile a gagné ce match dans l'historique
-            if match.htscore > match.atscore:
-                pourcentage_home -= 0.1  # Soustraire 0.1 si l'équipe à domicile a perdu ce match dans l'historique
-    
-    
-    # Influence du nombre de matchs gagnés
-    #pourcentage_home += 0.01 * (matchs_gagnes_home - matchs_gagnes_away)
-
-    # Influence du nombre de buts marqués
-    #pourcentage_home += 0.005 * (buts_marques_home - buts_marques_away)
-    
-    
-
-    # Assurer que le pourcentage est dans la plage [0, 1]
-    #pourcentage_home = max(0, min(1, pourcentage_home))
-
-    # Calculer le pourcentage pour l'équipe à l'extérieur
-    pourcentage_away = 1 - pourcentage_home
-
-    return pourcentage_home, pourcentage_away
 
 
 # Example usage:
 if __name__ == "__main__":
-    '''
+    
     id_odsp= 'newuFc20/'
     events_chosen = events[events['id_odsp'] == id_odsp]
     location_objects = extract_all_location(id_odsp)
     attack_distribition = calculate_attack_distribution(location_objects)
     print('attack distribution:')
     print(attack_distribition)
-
+    
+    pass_objects = extract_all_pass(location_objects)
+    pass_distribition = calculate_pass_distribution(pass_objects)
+    print('pass distribution:')
+    print(pass_distribition)
+    
     heat_map = calculate_heat_map(location_objects)
     print('heat map:')
     print(heat_map)
-    '''
+    
     
     name1 = 'Bordeaux'
     name2 = 'Lyon'
     match_history = extract_two_team(name1,name2)
     
-    [pourcentage_home, pourcentage_away] = pourcentage_victoire(name1, name2, match_history)
 
 
 
